5f30d5e64db2aade752708b3ee024eec3ce9f26b
Lana Lam <70384331+Lanalam731@users.noreply.github.com>
2021-03-24 09:07:50 -0500

    php

---
+++ index.php
@@ -0,0 +1,245 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Prim and Polished/home</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    </head>
+<body>
+  <!--Style for the navigation bar-->  
+  <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <nav class="navbar navbar-expand-lg">
+                     
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="navbar-toggler-icon"></span>
+                    </button> <a class="navbar-brand" href="index.html"><img src="img/logowhite1crop.png"></a>
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        
+                        <ul class="navbar-nav ml-md-auto">
+                            <li class="nav-item dropdown">
+                                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown">Menu</a>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+                                     <a class="dropdown-item" href="index.html">Home</a> <a class="dropdown-item" href="product.html">Products</a> <a class="dropdown-item" href="about.html">About Us</a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+<!--Style for the carousel-->
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-12">
+      <div class="carousel slide" id="carousel-362957">
+        <ol class="carousel-indicators">
+          <li data-slide-to="0" data-target="#carousel-362957" class="active">
+          </li>
+          <li data-slide-to="1" data-target="#carousel-362957">
+          </li>
+          <li data-slide-to="2" data-target="#carousel-362957">
+          </li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="d-block w-100" alt="Carousel Bootstrap First" src="https://www.layoutit.com/img/sports-q-c-1600-500-1.jpg" />
+            <div class="carousel-caption">
+              <h4>
+                First Thumbnail label
+              </h4>
+              <p>
+                Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+              </p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" alt="Carousel Bootstrap Second" src="https://www.layoutit.com/img/sports-q-c-1600-500-2.jpg" />
+            <div class="carousel-caption">
+              <h4>
+                Second Thumbnail label
+              </h4>
+              <p>
+                Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+              </p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" alt="Carousel Bootstrap Third" src="https://www.layoutit.com/img/sports-q-c-1600-500-3.jpg" />
+            <div class="carousel-caption">
+              <h4>
+                Third Thumbnail label
+              </h4>
+              <p>
+                Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+              </p>
+            </div>
+          </div>
+        </div> <a class="carousel-control-prev" href="#carousel-362957" data-slide="prev"><span class="carousel-control-prev-icon"></span> <span class="sr-only">Previous</span></a> <a class="carousel-control-next" href="#carousel-362957" data-slide="next"><span class="carousel-control-next-icon"></span> <span class="sr-only">Next</span></a>
+      </div>
+    </div>
+  </div>
+</div>
+<hr>
+<div class="container-fluid padding">
+	<div class="row padding">
+		<div class="col-lg-6">
+			<h2>Quality Nails</h2>
+       <p>You know what's sucky about regular press-on nail out of the store ? 
+         They arent reliable at all times and dont have the design you desire.
+          Here at Prim and Polished (storefront or website) we can guarantee you good quality nails in a variety of shapes and sizes. 
+          Prim and Polished allows customers to find their inner selves and pick nails that they have never tried before.
+          The shapes and sizes we offer are: square, coffin, and stilleto and the sizes are: short, medium, and long. 
+        Here at Prim and Polished we can guarantee our customers satisfaction and durable nails. </p>
+			<br>
+		</div>
+		<div class="col-lg-6">
+			<img src="img/nailsblank.jpeg" class="img-fluid">
+		</div>
+	</div>	
+	<hr class="my-4">
+	</div>
+<!--Style for the cards-->>
+<div class="container-fluid padding">
+  <div class="row welcome text-center">
+    <div class="col-12">
+      <h1 class="display-4">Nail lengths</h1>
+    </div>
+    <hr>
+  </div>	
+  </div>
+<div cllass="container-fluid padding">
+  <div class="row padding">
+    <div class="col-md-4">
+      <div class="card">
+        <img class="card-dash-img-top" src="img/nailsblank.jpeg">
+        <div class="card-body">
+          <h4 class="card-title">Short</h4>
+          <p class="card-text">
+            Blank
+          </p>
+          <a href="#" class="btn btn-outline-secondary">See Products</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card">
+        <img class="card-dash-img-top" src="img/nailsblank.jpeg">
+        <div class="card-body">
+          <h4 class="card-title">Medium</h4>
+          <p class="card-text">
+            Blank
+          </p>
+          <a href="#" class="btn btn-outline-secondary">See Products</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card">
+        <img class="card-dash-img-top" src="img/nailsblank.jpeg">
+        <div class="card-body">
+          <h4 class="card-title">Long</h4>
+          <p class="card-text">
+            Blank
+          </p>
+          <a href="#" class="btn btn-outline-secondary">See Products</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+  <div class="container-fluid padding">
+    <div class="row welcome text-center">
+      <div class="col-12">
+        <h1 class="display-4">Nail Shapes</h1>
+      </div>
+      <hr>
+    </div>	
+    </div>
+  <div cllass="container-fluid padding">
+    <div class="row padding">
+      <div class="col-md-4">
+        <div class="card">
+          <img class="card-dash-img-top" src="img/nailsblank.jpeg">
+          <div class="card-body">
+            <h4 class="card-title">Coffin</h4>
+            <p class="card-text">
+              Blank
+            </p>
+            <a href="#" class="btn btn-outline-secondary">See Products</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card">
+          <img class="card-dash-img-top" src="img/nailsblank.jpeg">
+          <div class="card-body">
+            <h4 class="card-title">Square</h4>
+            <p class="card-text">
+              Blank
+            </p>
+            <a href="#" class="btn btn-outline-secondary">See Products</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card">
+          <img class="card-dash-img-top" src="img/nailsblank.jpeg">
+          <div class="card-body">
+            <h4 class="card-title">Stilleto</h4>
+            <p class="card-text">
+              Blank
+            </p>
+            <a href="#" class="btn btn-outline-secondary">See Products</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+    <footer>
+      <div class="container-fluid padding">
+        <div class="row text-center">
+          
+          <div class="col-md-4">
+            <hr class="light">
+            <h5>Store Hours</h5>
+            <hr class="light">
+            <p>Monday-Thursday:11am-9pm</p>
+            <p>Friday-Saturday:11am-10pm</p>
+            <p>Sunday:closed</p>
+          </div>
+          <div class="col-md-4">
+            <img src="img/logowhite1crop.png">
+            <hr class="light">
+            <p>213-597-5555</p>
+            <p>ppnails@gmail.com</p>
+            <p> 263 Stay Pretty Dr</p>
+            <p>Los Angeles, CA, 9002</p>
+          </div>
+          <div class="col-md-4">
+            <hr class="light">
+            <h5>Store Fronts</h5>
+            <hr class="light">
+            <p>Macon, GA, 31201</p>
+            <p>Memphis, TN, 37501</p>
+            <p>Orlando, FL, 32789</p>
+            <p>Newyork, NY, 10001</p>
+          </div>
+          <div class="col-12">
+            <hr class="light-100">
+            <h5>&copy; PrimAndPolished.com</h5>
+          </div>
+        </div>
+      </div>
+    </footer>
+</body>
+
+</html>
---
+++ product.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Prim and Polished/products</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    </head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <nav class="navbar navbar-expand-lg">
+                     
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="navbar-toggler-icon"></span>
+                    </button> <a class="navbar-brand" href="index.html"><img src="img/logowhite1crop.png"></a>
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        
+                        <ul class="navbar-nav ml-md-auto">
+                            <li class="nav-item dropdown">
+                                 <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown">Menu</a>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+                                     <a class="dropdown-item" href="index.html">Home</a> <a class="dropdown-item" href="product.html">Products</a> <a class="dropdown-item" href="about.html">About Us</a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+    <!--page header-->
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="page-header">
+                    <h1>
+                        Prim and Polished <small>Products
+                </div>           
+    <!--carousel-->
+    <div class="product-carousel">
+                <div class="carousel slide" id="carousel-29568">
+                    <ol class="carousel-indicators">
+                        <li data-slide-to="0" data-target="#carousel-29568" class="active">
+                        </li>
+                        <li data-slide-to="1" data-target="#carousel-29568">
+                        </li>
+                        <li data-slide-to="2" data-target="#carousel-29568">
+                        </li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img class="d-block w-100" alt="Carousel Bootstrap First" src="img/nailsblank.jpeg" />
+                            <div class="carousel-caption">
+                                <h4>
+                                    First Thumbnail label
+                                </h4>
+                                <p>
+                                    Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" alt="Carousel Bootstrap Second" src="img/nailsblank.jpeg" />
+                            <div class="carousel-caption">
+                                <h4>
+                                    Second Thumbnail label
+                                </h4>
+                                <p>
+                                    Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" alt="Carousel Bootstrap Third" src="img/nailsblank.jpeg" />
+                            <div class="carousel-caption">
+                                <h4>
+                                    Third Thumbnail label
+                                </h4>
+                                <p>
+                                    Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                                </p>
+                            </div>
+                        </div>
+                    </div> <a class="carousel-control-prev" href="#carousel-29568" data-slide="prev"><span class="carousel-control-prev-icon"></span> <span class="sr-only">Previous</span></a> <a class="carousel-control-next" href="#carousel-29568" data-slide="next"><span class="carousel-control-next-icon"></span> <span class="sr-only">Next</span></a>
+                </div>
+            </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="conatiner-fluid padding">
+		<div class="row welcome text-center">
+			<div class="col-12">
+				<h1 class="display-4">See the Product</h1>
+				<hr>
+
+			</div>
+		</div>
+    </div>
+    <h1> Coffin </h1>
+    <button class="fun" data-toggle="collapse" data-target="#img">Nailshape#1
+	</button>
+	<div id="img" class="collapse">
+		<div class="container-fluid padding">
+			<div class="row text-center">
+				<div class="col-sm-6 col-md-3">
+					<img class="nail.blank" src="img/nailsblank.jpeg">
+				</div>
+				<div class="col-sm-6 col-md-3">
+					<img class="nail.blank" src="img/nailsblank.jpeg">
+				</div>
+				<div class="col-sm-6 col-md-3">
+					<img class="nail.blank" src="img/nailsblank.jpeg">
+				</div>
+				<div class="col-sm-6 col-md-3">
+					<img class="nail.blank" src="img/nailsblank.jpeg">
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
